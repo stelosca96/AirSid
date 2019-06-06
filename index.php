a3398c4922876c21d0b31b2b09f5df50849391a7
Stefano Loscalzo <stefano.loscalzo@gmail.com>
2019-06-06 20:22:23 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,117 @@
+<!doctype html>
+<html lang="it">
+<head>
+    <link rel="stylesheet" href="style2.css" type="text/css">
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+
+</head>
+<body>
+
+<?php
+    include "utility.php";
+    do_action();
+
+?>
+<header id="title_bar">
+    <h1>Seleziona un posto</h1>
+    <button id="loginBtn">Login</button>
+
+    <!-- Bottone login -->
+    <div id="login" class="modal">
+        <div class="modal-content">
+            <div class="modal-header">
+                <span class="close">&times;</span>
+                <h2 id="modalTitle"></h2>
+            </div>
+            <div class="modal-body" id="modalLogin">
+            <form id="formLogin" method="post" action="index.php">
+                <input class="loginInput" type="text" name="username" placeholder="Inserisci username" id="username">
+                <input class="loginInput" type="password" name="password" placeholder="Inserisci password" id="password">
+                <input type="hidden" name="action" value="login">
+                <input class="loginInput" type="submit" id="submitBtn" value="Accedi">
+            </form>
+                <button id="registrazioneBtn">Non sei registrato?</button>
+            </div>
+            <div class="modal-body" id="modalRegistrazione">
+                <form id="formRegistrazione">
+                    <input class="loginInput" type="text" name="username" placeholder="Inserisci username" id="username">
+                    <input class="loginInput" type="password" name="password" placeholder="Inserisci password"  id="password">
+                    <input class="loginInput" type="password" name="password_retype" placeholder="Riscrivi password"  id="password_retype">
+                    <input class="loginInput" type="submit" id="registerBtn" value="Registrati">
+                </form>
+            </div>
+        </div>
+    </div>
+    <script>
+        const modal = document.getElementById("login");
+        const modalLogin = document.getElementById("modalLogin");
+        const modalRegistrazione = document.getElementById("modalRegistrazione");
+        const modalTitle = document.getElementById("modalTitle");
+        const btnLogin = document.getElementById("loginBtn");
+        const btnRegistrazione = document.getElementById("registrazioneBtn");
+        const span = document.getElementsByClassName("close")[0];
+
+        btnLogin.onclick = function() {
+            modal.style.display = "block";
+            modalLogin.style.display = "block";
+            modalRegistrazione.style.display = "none";
+            modalTitle.textContent = "Login";
+        };
+
+        btnRegistrazione.onclick = function() {
+            modal.style.display = "block";
+            modalRegistrazione.style.display = "block";
+            modalTitle.textContent = "Registrazione";
+            modalLogin.style.display = "none";
+        };
+
+        span.onclick = function() {
+            modal.style.display = "none";
+            modalLogin.style.display = "none";
+            modalRegistrazione.style.display = "none";
+
+        };
+        window.onclick = function(event) {
+            if (event.target === modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
+</header>
+
+
+<aside id="menu">
+    <p>Menu</p>
+</aside>
+<section id="aereo">
+
+    <table id="fusoliera">
+            <?php
+        //la larghezza deve essere un numero pari
+        $larghezza = 6;
+        $lunghezza = 30;
+        for($y=1; $y<=$lunghezza; $y++){
+            echo "<tr class='sedili'>\n";
+            for($x=0; $x<$larghezza+1; $x++){
+                if($x == $larghezza/2)
+                    //Disegnare il corridoio
+                    echo "<td id='corridoio'></td>\n";
+                else{
+                    echo "<td><label class=\"container\">";
+                    echo "<input type=\"checkbox\" id=".$y.chr($x + 65).">";
+                    echo "<span class=\"checkmark\">".$y.chr($x + 65)."</span></label></td>\n";
+                }
+            }
+            echo "</tr>\n";
+        }
+
+        ?>
+    </table>
+
+</body>
+</html>
---
+++ utility.php
@@ -0,0 +1,84 @@
+<?php
+
+function db_connect(){
+    $conn = mysqli_connect("localhost", "root", "", "airsid");
+    if(!$conn){
+        my_redirect('Errore nella connessione(' .mysqli_connect_errno().')'.mysqli_connect_error());
+    }
+    return $conn;
+}
+
+function my_sanitize($insecure_string){
+    $secure_string = $insecure_string;
+    return $secure_string;
+}
+
+function  login(){
+    if(!(isset($_POST['username']) && isset($_POST['password'])))
+        my_redirect("Errore parametri");
+    $insecure_username = $_POST['username'];
+    $clear_password = $_POST['password'];
+
+    $conn = db_connect();
+    $password = sha1($clear_password);
+    //todo: Devo sanitizzare o no??
+//    $username = my_sanitize($insecure_username);
+    $username = mysqli_real_escape_string($conn, $insecure_username);
+    $query = "SELECT * FROM Users WHERE username=" . $username . " AND password=" . $password;
+    if(! $reply = mysqli_query($conn, $query))
+        my_redirect("Errore collegamento al DB");
+    if(mysqli_num_rows($reply)==0){
+        my_destroy_session();
+        my_redirect("Utente o password errata");
+    }
+    $row = mysqli_fetch_array($reply);
+
+    // todo: Controllo inutile lo faccio già in SQL
+    if($row['username']!=$username || $row['password']!=$password){
+        my_destroy_session();
+        my_redirect("Utente o password errata");
+    }
+    mysqli_close($conn);
+    session_start();
+    $_SESSION['user'] = $username;
+}
+
+function my_redirect($mex){
+    //todo: rimettere header.. Non worka bene, perchè??
+//    header('HTTP/1.1 307 temporary redirect');
+    header("Location: index.php?msg=".urlencode($mex));
+    exit;
+}
+
+function my_destroy_session(){
+    $_SESSION = array();
+    if(ini_get("session.use_cookies")){
+        $params = session_get_cookie_params();
+        setcookie(session_name(), '', time()-3600*24, $params["path"], $params["domain"], $params["secure"], $params["httponly"]);
+    }
+    session_destroy();
+}
+
+function user_exist(){
+
+}
+
+function registration(){
+
+}
+
+function do_action(){
+    if(isset($_REQUEST['action'])) {
+        switch ($_REQUEST['action']) {
+            case "login":
+                login();
+                break;
+            case "logout":
+                my_destroy_session();
+                break;
+            case "registration":
+                registration();
+                break;
+        }
+    }
+}
